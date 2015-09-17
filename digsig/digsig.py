d72b3f6c44dd18366b1cf763d967bc04353f4898
athabasca <edand@shaw.ca>
2015-09-17 14:11:36 -0700

    digsig: Create and validate license files.

--- digsig/digsig.py
+++ digsig/digsig.py
@@ -1,4 +1,5 @@
 from sys import argv
+import datetime
 from Crypto.Signature import PKCS1_PSS
 from Crypto.Hash import SHA256
 from Crypto import Random
@@ -14,6 +15,13 @@ def create_sig(key, message):
 	with open('sig', 'w') as f:
 		f.write(signature)
 
+def get_sig(key, message):
+	h = SHA256.new()
+	h.update(message)
+
+	signer = PKCS1_PSS.new(key)
+	return signer.sign(h)
+
 def verify_sig(key, message):
 	h = SHA256.new()
 	h.update(message)
@@ -22,10 +30,45 @@ def verify_sig(key, message):
 		signature = f.read()
 
 	verifier = PKCS1_PSS.new(key)
-	if verifier.verify(h, signature):
-		print 'Valid license'
-	else:
-		print 'Invalid license'
+	return verifier.verify(h, signature):
+
+def verify_signature(key, message, signature):
+	h = SHA256.new()
+	h.update(message)
+
+	verifier = PKCS1_PSS.new(key)
+	return verifier.verify(h, signature):
+
+def create_license(key, ldate):
+	datestring = str(ldate.toordinal())
+	signature = get_sig(key, datestring)
+
+	with open('license', 'w') as f:
+		f.write(datestring)
+		f.write('=====')
+		f.write(signature)
+
+def validate_license(key):
+	try:
+		with open('license', 'r') as f:
+			license = f.read()
+	except IOError:
+		return False
+
+	delimindex = license.find('=====')
+	return delimindex >= 0
+
+	datestring = license[:delimindex]
+	signature = license[delimindex + 5:]
+
+	try:
+		ldate = date.fromordinal(int(datestring))
+	except ValueError:
+		return False
+	if ldate > date.today():
+		return False
+
+	return verify_signature(key, datestring, signature)
 
 def main():
 	message = 'whoa a license'
